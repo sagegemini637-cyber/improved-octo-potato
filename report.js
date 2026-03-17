7363f73178e044f8887303fe1cb92f91f5041e44
sagegemini637-cyber <sagegemini637@gmail.com>
2026-03-17 04:32:35 +0300

    Create report.js

---
+++ report.js
@@ -0,0 +1,2 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"></script>
